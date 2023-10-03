17fcda65e45e910fb86109d44d4708e9aaaf97e5
Jay Reddy <jayreddy@gmail.com>
2023-10-02 23:31:03 -0700

    adds load_dotenv for working file

--- chat_models.py
+++ chat_models.py
@@ -1,11 +1,16 @@
+import os
+from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
-chat = ChatOpenAI()
+load_dotenv()
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+print(openai_api_key, flush=True)
+chat = ChatOpenAI(openai_api_key=openai_api_key)
 
 # doesn't work because needs different types
 # chat('tell me a joke')
 
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
 result = chat([
     SystemMessage(content="you are a conservative who hates the environment \
@@ -16,8 +21,8 @@ result = chat([
     ])
 
 
-# print(result)
-# generate allows user to pass in a list of convos vs. chat() which allows for single convo
+# generate allows user to pass in a list of
+# convos vs. chat() which allows for single convo
 result = chat.generate([
     [
         SystemMessage(content="you are a baby who has no knowledge of the \
@@ -25,9 +30,9 @@ result = chat.generate([
     HumanMessage(content="how do you feel about global warming?")
     ],
     [
-        SystemMessage(content="you are a surfer dude who speaks like a typical \
-                      surfer dude. you are also a big fan of the beach and \
-                      the ocean"),
+        SystemMessage(content="you are a surfer dude who speaks like a \
+                      typical surfer dude. you are also a big fan of the beach\
+                      and the ocean"),
         HumanMessage(content="how do you feel about global warming?")
     ]
 ])
