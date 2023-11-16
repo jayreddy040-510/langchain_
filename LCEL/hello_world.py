cba5343c7ba4669da3928a2547405c1aae9dd92f
Jay Reddy <jr@care.coach>
2023-11-15 19:17:15 -0800

    migrated to openai 1.x

--- LCEL/hello_world.py
+++ LCEL/hello_world.py
@@ -1,20 +1,23 @@
 import os
-from langchain.chat_models import AzureChatOpenAI
-from langchain.schema import HumanMessage
 from dotenv import load_dotenv
+from openai import AzureOpenAI
 
 load_dotenv()
 
-llm = AzureChatOpenAI(
-    openai_api_version="2023-05-15",
-    azure_deployment="carecoach-gpt35-16k"
+client = AzureOpenAI(
+  azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"), 
+  api_key=os.getenv("AZURE_OPENAI_KEY"),  
+  api_version="2023-05-15"
 )
 
-message = HumanMessage(
-    content="Translate this sentence from English to French. I love programming."
+response = client.chat.completions.create(
+    model="carecoach-gpt35-16k", # model = "deployment_name".
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        {"role": "user", "content": "Do other Azure AI services support this too?"}
+    ]
 )
 
-res = llm([message])
-
-print(res.content, flush=True)
-
+print(response)
