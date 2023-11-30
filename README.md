b6fdc96117ca16dec8ffc2bd2c7556929dee5b37
Jay Reddy <jayreddy@gmail.com>
2023-11-30 11:43:28 -0800

    wip

---
+++ README.md
@@ -0,0 +1,3 @@
+# openai/langchain sandbox for kids who can't read good
+
+#### ^it's a zoolander quote
--- basic_gen_script.py
+++
@@ -1,8 +0,0 @@
-from langchain.llms import OpenAI
-
-llm = OpenAI()
-
-# generate allows user to pass in a list of messages vs. llm() which allows for single message
-result = (llm.generate(['Here is a fun fact about Python programming:', 'Who let the dogs out?']))
-
-print(result.generations[1])
